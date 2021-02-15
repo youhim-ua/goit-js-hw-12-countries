7a4096ec866d77c2f5a6645d23497b62e746c4fd
Andrew Chepur-Krytskyi <66245509+youhim-ua@users.noreply.github.com>
2021-02-15 22:54:42 +0200

    small fixes

--- src/js/fetch-countries.js
+++ src/js/fetch-countries.js
@@ -14,33 +14,34 @@ const debouncedFetchCountries = _.debounce(fetchCountries, 500)
 
 refs.inputRef.addEventListener('input', event => debouncedFetchCountries(event.target.value));
 
+function fetchCountries(searchQuery) {
+    fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
+        .then(response => response.json())
+        .then((resultOfSearch) => buildMarkup(resultOfSearch))
+        .catch(() => {
+            error(errorOptionNotFound);
+        })
+};
+
 function removeContent() { 
     refs.searchedListRef.innerHTML = '';
     refs.searchBoxRef.innerHTML = '';
 }
 
-function fetchCountries(searchQuery) {
-    fetch(`https://restcountries.eu/rest/v2/name/${searchQuery}`)
-        .then(response => response.json())
-        .then((country) => {
-            if (country.length > 10) {
-                removeContent();
-                error(errorOption);
-            } else if (country.length === 1) {
-                removeContent();
-                const [{ name, capital, languages, population, flag }] = country;
-                const formattedArray = { name, capital, languages, population, flag };
-                refs.searchBoxRef.insertAdjacentHTML('beforeend', countryCardMarkup(formattedArray));
-            } else { 
-                removeContent();
-                const arrayOfCountryNames = country.map(item => `<li>${item.name}</li>`);
-                refs.searchedListRef.insertAdjacentHTML('beforeend', arrayOfCountryNames.join(''));
-            }
-            
-        })
-        .catch(() => {
-            removeContent();
-            error(errorOptionNotFound);
-        })
-};
+function buildMarkup(countryList) {
+    if (countryList.length > 10) {
+        removeContent();
+        error(errorOption);
+    } else if (countryList.length === 1) {
+        removeContent();
+        const [{ name, capital, languages, population, flag }] = countryList;
+        const formattedArray = { name, capital, languages, population, flag };
+        refs.searchBoxRef.insertAdjacentHTML('beforeend', countryCardMarkup(formattedArray));
+    } else { 
+        removeContent();
+        const arrayOfCountryNames = countryList.map(item => `<li>${item.name}</li>`);
+        refs.searchedListRef.insertAdjacentHTML('beforeend', arrayOfCountryNames.join(''));
+    }
+}
+
 
